eee957d1bc0b6d5eda3b604603b2691a4b2118b2
tim-hub <tim_w@outlook.com>
2020-08-11 22:18:30 +1200

    add a proper heavy work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import pambda from 'pambdajs';
-
+// import { getData } from "./io.js";
 
 const main = async () => {
-  const p = pambda.init()
+  const data = Array.from({length: 100000}, (x, i) => i);
+  const p = pambda.init(4)
+  console.log(p.processCount, 'processes')
 
-  const data = [1, 2, 3]
-  await p.map((x) => {
-    console.log(x)
-  }, data)
 
-  data.map(d => {
-    console.log(d)
-  })
+  const cb = (x) => {
+    // this is a heavy work
+    let sum = 0
+    for (let i = 0; i < 100000; i++) {
+      sum += i + x;
+    }
+    return sum;
+  }
+
+
+  console.time('multi process')
+  const pr = await p.map(cb, data)
+  console.timeEnd('multi process')
+
+
+  console.time('single process')
+  const r = data.map(cb)
+  console.timeEnd('single process')
+
 }
 
 main();
---
+++ src/io.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+
+export const getData = (filePath) => {
+  let thePath = '';
+  if (filePath) {
+    thePath = filePath
+  } else {
+    thePath = 'src/data/5000lines.json'
+  }
+  const data = JSON.parse(fs.readFileSync(thePath, 'utf8'))
+  return data
+}
